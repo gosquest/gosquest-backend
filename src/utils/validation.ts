d922c28a5f449ac89b70518bebb7efe00920f141
Peace-Ishimwe <peaceishimwem@gmail.com>
2024-10-07 15:10:51 +0200

    feat: feat: adding projects

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -47,7 +47,7 @@ export const validateProjectDto = (ProjectDto: any) => {
     name: Joi.string().min(3).max(100).required(),
     fields: Joi.array().min(1).max(100).required(),
     team_leader: Joi.string().required(),
-    description: Joi.string().min(10).max(500).required(),
+    description: Joi.string().min(10).max(1000).required(),
     logo: Joi.string().required(),
     cover_image: Joi.string().required(),
     link: Joi.string().required()
